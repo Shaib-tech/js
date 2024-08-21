91ce12670b24b0c0e2bcb98511c72d93613a743e
Shaib-tech <shaib.platinuminfos@gmail.com>
2024-08-21 11:13:18 +0000

    done

---
+++ .devcontainer/01_Basics/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail ="shaib@gmail.com"
+var accountPassword ="12345"
+accountCity="Lucknow"
+let accountState;
+
+// accountId =2 // Not Allowed 
+console.log(accountId);
+
+
+ accountEmail="zayed@gmail.com"
+ accountPassword="225456698"
+ accountCity="Jaipur"
+
+ console.table([accountId,accountEmail, accountPassword, accountCity, accountState])
