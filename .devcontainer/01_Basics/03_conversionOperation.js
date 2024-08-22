8a58ed86b06d1f344f6ac39cc3675d8b914e0240
Shaib-tech <shaib.platinuminfos@gmail.com>
2024-08-22 09:06:35 +0000

    Done

--- .devcontainer/01_Basics/03_conversionOperation.js
+++ .devcontainer/01_Basics/03_conversionOperation.js
@@ -29,3 +29,52 @@ let someNumber = 20
 let someString = String (someNumber)
 console.log(someString);
 console.log(typeof someString);
+
+
+// ****************************** OPERATIONS *************************************** \\
+
+let num = 3
+let negNumber = -value
+console.log(negNumber);
+
+// console.log (2+2);
+// console.log (2-2);
+// console.log (2*2);
+// console.log (2**2);
+// console.log (2/5);
+// console.log (2%3);
+
+
+
+let str1 = "Hello"
+let str2 = " Shaib"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+console.log(2 + 3 * 5 % 3);
+
+
+console.log(+true);
+
+console.log(+"");
+
+// let num1, num2, num3
+//  num1 = num2 = num3 = 2 + 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+let gameCount = 100
+++gameCount;
+console.log(gameCounter);
+
+let x = 3;
+const y = ++x;
+console.log(x);
