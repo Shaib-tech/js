2d68290907d1e8df421bc63bbc6a2c68040f791e
Shaib-tech <shaib.platinuminfos@gmail.com>
2024-08-23 07:00:30 +0000

    Complete

--- .devcontainer/01_Basics/05_Datatypes-Summary.js
+++ .devcontainer/01_Basics/05_Datatypes-Summary.js
@@ -12,30 +12,57 @@
 
 // let userEmail;
 
-const id = Symbol('123');
+// const id = Symbol('123');
 
-const anotherId = Symbol('123');
+// const anotherId = Symbol('123');
 
-console.log(id == anotherId);
+// console.log(id == anotherId);
 
-const bigNum = 12168485858484848656n
+// const bigNum = 12168485858484848656n
 
 /* Non Primitive (Reference) Has 3 Types
  Arrays, Objects, Functions
 */
 
-const heros = ["Shaktiman", "Balveer", "Hatim"]
+// const heros = ["Shaktiman", "Balveer", "Hatim"]
 
-const obj = {
+// const obj = {
 
-    name : "Shaib",
-    age : 23,
-    city : "Lucknow"
-}
+//     name : "Shaib",
+//     age : 23,
+//     city : "Lucknow"
+// }
 
-const myFunction = function(){
-    console.log("Hello World");
+// const myFunction = function(){
+//     console.log("Hello World");
     
+// }
+
+// console.log(typeof obj);
+
+// **********************************************
+// Stack (Primitive), Heap (Non-Primitive) 
+
+// Example of Stack Memory Allocation ////////////
+
+let myName = "Shaib Usmani"
+let anotherName = myName
+
+anotherName ="Zayed Usmani"
+
+console.log(anotherName);
+console.log(myName);
+///////////////////////////////////////////////////
+
+// Example of Heap Memory Allocation //////////////
+
+let userOne = {
+    name:"Shaib",
+    upi: "user@gmail.com"
 }
 
-console.log(typeof obj);
+let userTwo = userOne
+userTwo.upi = "ok@gpay"
+console.log(userTwo);
+
+////////////////////////////////////////////////////
