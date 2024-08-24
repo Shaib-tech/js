fc2a684bb78b79775f0d43e95ab19da2cd23a42d
Shaib-tech <shaib.platinuminfos@gmail.com>
2024-08-24 16:10:43 +0000

    Array 01 Done

---
+++ .devcontainer/02_Basics/01_arrays.js
@@ -0,0 +1,41 @@
+/////// Arrays ///////
+
+const myArr = [0,1,2,3,4,5]
+
+const heroes = ["Shaib", "Zayed"]
+
+const myArr1 = new Array (10,20,30)
+// console.log(myArr1[2]);
+
+//////// Arrays Methods //////////
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop ()
+
+// myArr.unshift(10)
+// myArr.shift()
+// console.log(myArr.includes(10));
+// console.log(myArr.indexOf(0));
+
+// const newArr = myArr.join()
+// console.log(newArr)
+// console.log( typeof newArr)
+
+
+console.log(" A",myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+
+console.log(" B", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(" C",myArr);
+
+console.log(myn2);
+
+
+
+
+
