a829f405a4848d779dac1932b2c7376d3f397b3a
Shaib-tech <shaib.platinuminfos@gmail.com>
2024-08-29 17:38:25 +0000

    Global And Block Scope Basics

---
+++ .devcontainer/03_Basics/02_scopes.js
@@ -0,0 +1,17 @@
+// Global scope example
+
+let a = 300
+
+
+if (true){
+    let a = 10;
+    const b = 20;
+     var c = 30;
+     console.log("INNER SCOPE :",a);
+     
+} // Block scope example 
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+//////////////////////////////////////
