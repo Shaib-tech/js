0f109c079702c6a7042a2c83ec99797de72d753a
Shaib-tech <shaib.platinuminfos@gmail.com>
2024-08-30 09:26:57 +0000

    Advance Scopes and Function declaration

--- .devcontainer/03_Basics/02_scopes.js
+++ .devcontainer/03_Basics/02_scopes.js
@@ -7,11 +7,55 @@ if (true){
     let a = 10;
     const b = 20;
      var c = 30;
-     console.log("INNER SCOPE :",a);
+    //  console.log("INNER SCOPE :",a);
      
 } // Block scope example 
 
 // console.log(a);
 // console.log(b);
-console.log(c);
-//////////////////////////////////////
+// console.log(c);
+
+/////////////////// Nested Scope     ///////////////////
+
+function one (){
+    const username = "Shaib"
+
+    function two (){
+        const website = "Youtube"
+        
+        // console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+one()
+
+
+if (true){
+    const username ="Shaib"
+    if(username === "Shaib"){
+        const website =" GitHub"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+//************* Interesting ***************//
+
+/// In the idustry, you can see two type of function syntax.
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+//////////////////////////
+addtwo(5)
+
+const addtwo = function(num){
+    return num + 2
+}
+
